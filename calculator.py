6769eace6afb4ecca495f8ad60c06d59d08faa5c
sairaghav44 <sairaghav.dev@gmail.com>
2026-09-08 10:04:40 +0530

    Day 5: Python calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#calculator v1.0
+
+operator = input("enter  a operator (+,-,*,/): ")
+
+num1 = float(input("enter the 1st number : "))
+num2 = float(input("enter the 2nd number : "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print (f"Entered "{operator}" is not a valid operator!")
